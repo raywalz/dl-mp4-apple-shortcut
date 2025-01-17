c05a9dc9b0803a8e8d9e196356a8cc9cdb705ddf
Ray Walz <33192362+raywalz@users.noreply.github.com>
2025-01-17 17:21:55 -0500

    Correct upgrade command

--- install-dl-mp4.sh
+++ install-dl-mp4.sh
@@ -4,7 +4,8 @@ cd yt-dlp
 mkdir temp
 cat > dl-mp4.sh << EOF
 rm ~/Documents/yt-dlp/temp/*
-yt-dlp -U
+pip install --upgrade pip
+pip install --upgrade yt-dlp
 yt-dlp \$1 -f "bestvideo[ext=mp4][vcodec=h264]+bestaudio[ext=m4a][acodec=aac]/mp4" --recode-video mp4 --restrict-filenames --ignore-error --no-mtime -P ~/Documents/yt-dlp/temp -o '%(title)s-%(id)s.%(ext)s' --write-sub --write-auto-sub --sub-lang "en.*" --embed-subs
 open shortcuts://run-shortcut?name=Download%20MP4
 EOF
